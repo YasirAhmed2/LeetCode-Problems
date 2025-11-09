d52fb2349595ab794565662909ffe3724047d260
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-11-09 21:24:20 +0500

    Permutation problem solved

---
+++ permutations.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+
+        def backtrack(path, remaining):
+            if not remaining:
+                res.append(path)
+                return
+            for i in range(len(remaining)):
+                backtrack(path + [remaining[i]], remaining[:i] + remaining[i+1:])
+
+        backtrack([], nums)
+        return res
+
+
+# Example usage:
+solution = Solution()
+print(solution.permute([1, 2, 3]))
