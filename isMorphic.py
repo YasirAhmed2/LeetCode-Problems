96f547c6dbf3294d1fab3d8b64a28a3a5edeb6d8
YasirAhmed2 <yasirahmed5077@gmail.com>
2025-11-10 22:25:43 +0500

    isomorphic problem solved

--- isMorphic.py
+++ isMorphic.py
@@ -10,3 +10,49 @@
 # Mapping 'e' to 'a'.
 # Mapping 'g' to 'd'.
 # solution using python
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # If lengths differ, they can never be isomorphic
+        if len(s) != len(t):
+            return False
+        
+        # Dictionaries to keep track of mappings from s->t and t->s
+        map_s_t = {}
+        map_t_s = {}
+        
+        for ch_s, ch_t in zip(s, t):
+            # Check existing mapping from s to t
+            if ch_s in map_s_t:
+                if map_s_t[ch_s] != ch_t:
+                    return False
+            else:
+                map_s_t[ch_s] = ch_t
+            
+            # Check existing mapping from t to s (to prevent duplicate mapping)
+            if ch_t in map_t_s:
+                if map_t_s[ch_t] != ch_s:
+                    return False
+            else:
+                map_t_s[ch_t] = ch_s
+        
+        return True
+
+
+# --- Example Usage ---
+sol = Solution()
+
+# Example 1
+s1 = "egg"
+t1 = "add"
+print(f"Is '{s1}' and '{t1}' isomorphic? ->", sol.isIsomorphic(s1, t1))  # True
+
+# Example 2
+s3="hello"
+s4="billion"
+print(f"Is '{s3}' and '{s4}' isomorphic? ->", sol.isIsomorphic(s3, s4))  # False
